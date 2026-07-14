f10c8366d78c833c503c82ab6e2e783349023449
Wardcy9 <jimancardo@gmail.com>
2026-07-14 20:20:50 -0300

    Fix product config and add recovery partition size

--- twrp_herolte.mk
+++ twrp_herolte.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 PRODUCT_DEVICE := herolte
 PRODUCT_NAME := twrp_herolte
